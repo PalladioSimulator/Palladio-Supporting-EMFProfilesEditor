7c486d908e55263764ed84a1bc237b39d9ad6b26
Amine Kechaou <aminekechaou@gmail.com>
2018-02-15 12:40:31 +0100

    Buckminster project

--- emfprofiles.design/src/emfprofiles/design/Services.java
+++ emfprofiles.design/src/emfprofiles/design/Services.java
@@ -3,17 +3,12 @@ package emfprofiles.design;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.presentation.EcoreActionBarContributor.ExtendedLoadResourceAction.TargetPlatformPackageDialog;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.sirius.diagram.DSemanticDiagram;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.modelversioning.emfprofile.Extension;
@@ -42,23 +37,19 @@ public class Services {
     }
     
     public EPackage selectEPackage(EObject self) {
-    	System.out.println("test ");
     	Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
         TargetPlatformPackageDialog classpathPackageDialog = new TargetPlatformPackageDialog(shell);
         classpathPackageDialog.setMultipleSelection(false);
         EPackage result = null;
         if (classpathPackageDialog.open() == Dialog.OK) {
-            //Object [] result = classpathPackageDialog.getResult();
             String uri = classpathPackageDialog.getFirstResult().toString();
         	result = EPackage.Registry.INSTANCE.getEPackage(uri);
         }
-        /*Session session = SessionManager.INSTANCE.getSession(self);
-        session.addSemanticResource(result.eResource().getURI(), new NullProgressMonitor());*/
         return result;
     }
     
-    public EObject print(EObject obj) {
+    /*public EObject print(EObject obj) {
     	System.out.println(obj);
     	return obj;
-    }
+    }*/
 }
